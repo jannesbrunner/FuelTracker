5a1db6a072472287c710a15ffb18c453de73c1dd
Jannes Brunner <mail@jannesbrunner.de>
2022-01-08 15:09:08 +0100

    refactore: switch to class in app.js

--- App.js
+++ App.js
@@ -1,13 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import AppNavigator from './js/AppNavigator';
+
+export default class App extends Component {
+  render() {
+    <AppNavigator />;
+  }
 }
 
 const styles = StyleSheet.create({
