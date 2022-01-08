992b4b8c24a733054d2bb9df935ec2377bdb858d
Jannes Brunner <mail@jannesbrunner.de>
2022-01-08 15:10:43 +0100

    feat: add skeleton screen classes

---
+++ js/AppNavigator.js
@@ -0,0 +1,17 @@
+// Module imports
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+
+// Screen imports
+import MainScreen from './screens/home';
+import SettingsScreen from './screens/settings';
+import StatisticsScreen from './screens/statistics';
+import TrackFuelScreen from './screens/trackFuel';
+
+export default function AppNavigator() {
+    return(
+        <NavigationContainer>
+            
+        </NavigationContainer>
+    )
+}
