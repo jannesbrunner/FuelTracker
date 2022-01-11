3999bc2cff71754b3558eac25668c9b16eceb00e
Jannes Brunner <mail@jannesbrunner.de>
2022-01-11 19:08:17 +0100

    func: add kilometer input with callback function

--- js/screens/fueling/addKilometers.js
+++ js/screens/fueling/addKilometers.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Button, KeyboardAvoidingView } from 'react-native';
 
 export default class AddKilometersScreen extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { kilometers: 0 }
+    }
+    
+    handleInput(input) {
+        console.log(input);
+        this.setState({ kilometers: input.toString()}) 
+    }
+    
     render() {
         const { navigation } = this.props
         return (
-            <View styles={styles.container}>
+            <KeyboardAvoidingView styles={styles.container}>
                 <Text style={styles.text}>Add Total Kilometer</Text>
+                <TextInput 
+                    style={styles.textInput}
+                    value={this.state.kilometers.toString()}
+                    keyboardType={'numeric'}
+                    maxLength={7}  
+                    onChangeText={(input) => this.handleInput(input)}
+                ></TextInput>
                 <Button 
                     onPress={() => navigation.push('addPricePerLiter')} 
                     title={'Next: Add Price Per Liter'}
                     >
                 </Button>
-            </View>
+            </KeyboardAvoidingView>
         );
     }
 }
@@ -26,5 +43,11 @@ const styles = StyleSheet.create({
     },
     text: {
         fontSize: 50,
+    },
+    textInput: {
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
     }
 })
