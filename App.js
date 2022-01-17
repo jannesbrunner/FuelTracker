49c016eb0cec722a9f7dc06721fc4e53ad9076e9
Jannes Brunner <mail@jannesbrunner.de>
2022-01-17 16:53:18 +0100

    rewrite to function component

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component, createContext } from 'react';
+import { ThemeProvider } from "react-native-rapi-ui";
+import { View } from 'react-native';
+import { useAuth, AuthProvider } from './js/helpers/auth'
 import AppNavigator from './js/AppNavigator';
+import AuthNavigator from './js/AuthNavigator';
 
-export default class App extends Component {
-  render() {
-    return (
-      <AppNavigator />
-    );
-  }
+export default function App() {
+
+  return (
+    <ThemeProvider theme="light">
+      <AuthProvider>
+        <AppNavigator />
+      </AuthProvider>
+    </ThemeProvider>
+  );
+
 }
