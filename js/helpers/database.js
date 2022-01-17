3018d2248fbc0806cc6e87f6af7c8157bab308c5
Jannes Brunner <mail@jannesbrunner.de>
2022-01-17 16:55:20 +0100

    add supabase config

---
+++ js/helpers/database.js
@@ -0,0 +1,11 @@
+import { createClient } from '@supabase/supabase-js'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
+import db_creds from './db_creds'; // Not part of the repo!
+
+const supabaseUrl = db_creds.supabaseUrl;
+const supabaseAnonKey = db_creds.supabaseAnonKey;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+    localStorage: AsyncStorage,
+  })
