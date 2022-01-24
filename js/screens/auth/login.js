3dcc5a0e16a27e104a4438d6a19f5a943f1eac72
s86817 <s86817@bht-berlin.de>
2022-01-24 10:58:58 +0100

    designed login screen (not final)

--- js/screens/auth/login.js
+++ js/screens/auth/login.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { StyleSheet, View } from 'react-native';
-import { Layout, Text, TopNav, Button, TextInput } from 'react-native-rapi-ui';
+import { Layout, Text, TopNav } from 'react-native-rapi-ui';
 import { Ionicons } from '@expo/vector-icons';
+import { Button, TextInput, HelperText } from 'react-native-paper';
 
 export default class LoginScreen extends Component {
     constructor(props) {
@@ -9,6 +10,10 @@ export default class LoginScreen extends Component {
         this.state = {
             userEmail: '',
             userPassword: '',
+
+            visible: true,
+
+            falsePasswordOrUser: false //to be implemented
         }
     }
 
@@ -16,49 +21,78 @@ export default class LoginScreen extends Component {
         const { navigation } = this.props
         const { userEmail, userPassword } = this.state
         return (
-            <Layout>
-                <TopNav middleContent="Fuel Tracker 0.1" />
-                <Text style={styles.text}>Login</Text>
-                <TextInput
-                    
-                    autoCapitalize="none"
-                    placeholder="Enter your eMail"
-                    value={userEmail}
-                    onChangeText={(val) => this.setState({userEmail: val})}
-                    leftContent={
-                        <Ionicons name="mail" size={20} color={'grey'} />
-                    }
-                />
-                <TextInput
-                    style={{ marginBottom: 100 }}
-                    autoCapitalize="none"
-                    placeholder="Enter your password"
-                    secureTextEntry={true}
-                    value={userPassword}
-                    onChangeText={(val) => this.setState({userPassword})}
-                    leftContent={
-                        <Ionicons name="lock-closed" size={20} color={'grey'} />
-                    }
-                />
-                <Button
-                    onPress={() => alert('Logging you in...')}
-                    text="Login"
-                    status="primary"
-                >
-                </Button>
-                <View
-                    style={{
-                        borderBottomColor: 'black',
-                        borderBottomWidth: 1,
-                    }}
-                />
-                <Button
-                    onPress={() => navigation.push('register')}
-                    text={'Register'}
-                    status="info"
-                >
-                </Button>
-            </Layout>
+            <View style={styles.container}>
+                <Layout>
+                    <TopNav middleContent="Fuel Tracker 0.1" />
+                    <Text style={styles.text}>Login</Text>
+                    <View style={styles.inputView}>
+                        <TextInput
+
+                            autoCapitalize="none"
+                            placeholder="Enter your eMail"
+                            value={userEmail}
+                            mode="outlined"
+                            activeOutlineColor="#00a400"
+                            onChangeText={(val) => this.setState({ userEmail: val })}
+                            leftContent={
+                                <Ionicons name="mail" size={20} color={'grey'} />
+                            }
+                            error={this.state.falsePasswordOrUser}
+                        />
+                    </View>
+                    <View style={styles.inputView}>
+                        <TextInput
+                            style={{ marginBottom: 100 }}
+                            autoCapitalize="none"
+                            placeholder="Enter your password"
+                            secureTextEntry={this.state.visible}
+                            value={userPassword}
+                            mode="outlined"
+                            activeOutlineColor="#00a400"
+                            onChangeText={(val) => this.setState({ userPassword: val })}
+                            leftContent={
+                                <Ionicons name="lock-closed" size={20} color={'grey'} />
+                            }
+                            right={
+                                <TextInput.Icon
+                                    name="eye"
+                                    onPress={() => {
+                                        this.setState({ visible: !(this.state.visible) });
+                                    }}
+                                />
+                            }
+                            error={this.state.falsePasswordOrUser}
+                        />
+                    </View>
+
+                    <HelperText type="error" visible={this.state.falsePasswordOrUser}>
+                        E-Mail-Adresse oder Passwort ist falsch
+                    </HelperText>
+
+                    <Button
+                        onPress={() => alert('Logging you in...')}
+                        text="Login"
+                        status="primary"
+                        style={styles.loginBtn}
+                        mode="outlined"
+                        color="FFFFFF"
+                    >
+                    </Button>
+                    <View
+                        style={{
+                            borderBottomColor: 'black',
+                            borderBottomWidth: 1,
+                        }}
+                    />
+                    <Button
+                        onPress={() => navigation.push('register')}
+                        text={'Register'}
+                        status="info"
+                        style={styles.register_button}
+                    >
+                    </Button>
+                </Layout>
+            </View>
         );
     }
 }
@@ -72,5 +106,25 @@ const styles = StyleSheet.create({
     },
     text: {
         fontSize: 50,
-    }
+    },
+    inputView: {
+        borderRadius: 10,
+        width: "70%",
+        height: 45,
+        marginBottom: 20,
+        alignItems: "center",
+    },
+    loginBtn: {
+        borderRadius: 10,
+        height: 50,
+        alignItems: "center",
+        justifyContent: "center",
+        marginTop: 20,
+        backgroundColor: "#00a400",
+    },
+    register_button: {
+        height: 30,
+        marginTop: 10,
+        color: "#808080",
+    },
 })
