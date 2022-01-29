ee7c673e6e60e328b0b7e69c4f9534109e763839
Jannes Brunner <mail@jannesbrunner.de>
2022-01-29 18:04:16 +0100

    first data base fetch

--- js/screens/manageFuels.js
+++ js/screens/manageFuels.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
+import { supabase } from '../helpers/database';
+
 
 export default class manageFuelsScreen extends Component {
     
+    constructor(props) {
+        super(props);
+        this.state = { 
+            fuels: [],
+            error: null    
+        }
+    }
+
     render() {
         const { navigation } = this.props
+        const { error, fuels} = this.state
+        console.log(fuels);
         return (
             <View styles={styles.container}>
                 <Text style={styles.text}>Manage Fuels</Text>
-                <Text>No refueling tracked yet</Text>
+                {error ? <Text>There was an error: {error} </Text> : <Text>Your recorded fuelings:</Text>}
+                {fuels.length > 0 ? <Text>{fuels[0].mileage}</Text> : <Text>No fuelings yet</Text> }
+                
+                
                 <Button 
                     onPress={() => navigation.goBack()} 
                     title={'Back'}
@@ -17,6 +32,28 @@ export default class manageFuelsScreen extends Component {
                 </Button>
             </View>
         );
+    }
+
+    async getFuelings() {
+            const { data, error } = await supabase
+            .from('refueling')
+            .select();
+            if(error) {
+                console.log(error);
+                this.setState({
+                    fuels: [],
+                    error: error.message
+                })
+            } else {
+                console.log(data);
+                this.setState({
+                    fuels: data
+                })
+            }
+    }
+
+    componentDidMount() {
+       this.getFuelings();
     }
 }
 
