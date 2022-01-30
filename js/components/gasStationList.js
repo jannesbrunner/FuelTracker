5f6715bcc63b7d5ec6b18b121e57b714ad13003a
Jannes Brunner <mail@jannesbrunner.de>
2022-01-30 18:20:49 +0100

    Udate item to new DB structure

--- js/components/gasStationList.js
+++ js/components/gasStationList.js
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from "react";
-import { FlatList, SafeAreaView, StatusBar, StyleSheet, Text, TouchableOpacity } from "react-native";
+import { Platform, FlatList, SafeAreaView, StatusBar, StyleSheet, Text, TouchableOpacity } from "react-native";
+
+import * as Location from 'expo-location';
 
 import { supabase } from '../helpers/database';
 
 const Item = ({ item, onPress, backgroundColor, textColor }) => (
     <TouchableOpacity onPress={onPress} style={[styles.item, backgroundColor]}>
-        <Text style={[styles.title, textColor]}>{item.name}</Text>
-        <Text>{item.location}</Text>
+        <Text style={[styles.title, textColor]}>{item.company}</Text>
+        <Text>{item.address}</Text>
     </TouchableOpacity>
 );
 
@@ -14,28 +16,62 @@ const gasStationList = (props) => {
     const [selectedId, setSelectedId] = useState(null);
     const [stations, setStations] = useState(null);
     const [error, setError] = useState(null);
+    const [location, setLocation] = useState(null);
+    const [errorMsg, setErrorMsg] = useState(null);
+
+    // get current geo location to sort list to nearest station
+    useEffect(() => {
+        (async () => {
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== 'granted') {
+                setErrorMsg('Permission to access location was denied');
+                return;
+            }
+
+            let location = await Location.getCurrentPositionAsync({});
+            setLocation(location);
+            console.log(location);
+
+        })();
+        
+        // sort stations according to geo location
+        if(stations) {
+                console.log(stations);
+        }
+    }, [stations]); // run if stations get updated
+
+    let text = 'Waiting..';
+    if (errorMsg) {
+        text = errorMsg;
+    } else if (location) {
+        text = JSON.stringify(location);
+    }
+
+
 
     // retrieve data from DB
-    useEffect( () => { // component did mount
+    useEffect(() => { // component did mount
         async function getData() {
             const { data, error } = await supabase
-            .from('gas_station')
-            .select()
+                .from('gas_station')
+                .select()
             return { data, error }
         }
-        getData().then( (response) => {
-            if(response.data) {
+        getData().then((response) => {
+            if (response.data) {
                 setStations(response.data);
                 console.log(response.data);
             } else {
                 console.log(error);
                 setError(response.error.message);
             }
-        }).catch( error => {
+        }).catch(error => {
             console.log(error);
             setError(error.toString());
         });
     }, [])
+
+
 
     const renderItem = ({ item }) => {
         const backgroundColor = item.id === selectedId ? "blue" : "grey";
@@ -59,8 +95,8 @@ const gasStationList = (props) => {
                 keyExtractor={(item) => item.id}
                 extraData={selectedId}
             /> : <Text>Error: {error}</Text>}
-            
-            
+
+
         </SafeAreaView>
     );
 };
