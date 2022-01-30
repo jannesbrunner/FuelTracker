e15212fc61af07c73adc9b220466758142d970d1
Jannes Brunner <mail@jannesbrunner.de>
2022-01-30 22:34:57 +0100

    ref: put logic into parent component

--- js/components/gasStationList.js
+++ js/components/gasStationList.js
@@ -1,10 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { FlatList, SafeAreaView, StatusBar, StyleSheet, Text, TouchableOpacity } from "react-native";
 
-import * as Location from 'expo-location';
 
-import { supabase } from '../helpers/database';
-import { sortStationsByLocation } from "../helpers/snippets";
 
 const Item = ({ item, onPress, backgroundColor, textColor }) => (
     <TouchableOpacity onPress={onPress} style={[styles.item, backgroundColor]}>
@@ -14,63 +11,7 @@ const Item = ({ item, onPress, backgroundColor, textColor }) => (
 );
 
 const gasStationList = (props) => {
-    const [selectedId, setSelectedId] = useState(null);
-    const [stations, setStations] = useState(null);
-    const [errorMsg, setErrorMsg] = useState(null);
-    const [location, setLocation] = useState(null);
-
-    // get stations from db, then get user location, then sort stations by locations
-    useEffect(() => {
-        (async () => {
-            try {
-                const { body, error, status } = await getData();
-                if(error) {
-                    throw new Error(`${error.message} - ${status}`)
-                }
-                const stations = body;
-                console.log(stations);
-                const gpsLocation = await getCurrentLocation();
-               
-                if(gpsLocation) {
-                    const sortedStations = sortStationsByLocation(stations, gpsLocation);
-                    setStations(sortedStations);
-                    setLocation(gpsLocation);
-                } else {
-                    setStations(stations);
-                }
-
-            } catch (error) {
-                console.log(error);
-                setErrorMsg(error.toString());
-            }
-        })();
-    }, []); // run like componentDidMount
-
-    async function getCurrentLocation() {
-        try {
-            let { status } = await Location.requestForegroundPermissionsAsync();
-        if (status !== 'granted') {
-            setErrorMsg('Permission to access location was denied');
-            return false;
-        }
-
-        let location = await Location.getCurrentPositionAsync({});
-        return location;
-        } catch (error) {
-            return error;
-        }
-    };
-    
-    async function getData() {
-        try {
-            const data = await supabase
-            .from('gas_station')
-            .select()
-        return data
-        } catch (error) {
-            return error
-        }
-    }
+    const [selectedId, setSelectedId] = useState(null);        
 
     const renderItem = ({ item }) => {
         const backgroundColor = item.id === selectedId ? "blue" : "grey";
@@ -88,12 +29,12 @@ const gasStationList = (props) => {
 
     return (
         <SafeAreaView style={styles.container}>
-            {!errorMsg ? <FlatList
-                data={stations}
+          <FlatList
+                data={props.stations}
                 renderItem={renderItem}
                 keyExtractor={(item) => item.id}
                 extraData={selectedId}
-            /> : <Text>{errorMsg}</Text>}
+           />
         </SafeAreaView>
     );
 };
--- js/screens/fueling/addStation.js
+++ js/screens/fueling/addStation.js
@@ -1,25 +1,98 @@
 import React, { Component } from 'react';
-import { Button, KeyboardAvoidingView, StyleSheet, Text } from 'react-native';
+import { View, Button, KeyboardAvoidingView, StyleSheet, Text } from 'react-native';
+
+import * as Location from 'expo-location';
+
+import { supabase } from '../../helpers/database';
+import { sortStationsByLocation } from "../../helpers/snippets";
 
 import GasStationList from '../../components/GasStationList';
 
 export default class AddStation extends Component {
     constructor(props) {
         super(props);
-        this.state = { selectedStation: null }
+        this.state = { 
+            selectedStation: null,
+            stations: null,
+            location: null,
+            errorMsg: null
+        }
     }
 
+    componentDidMount() {
+        // get stations from db, then get user location, then sort stations by location
+        this.init();
+    }
+
+    async init() {
+        try {
+            const { body, error, status } = await this.getData();
+            if(error) {
+                throw new Error(`${error.message} - ${status}`)
+            }
+            const stations = body;
+            console.log(stations);
+            const gpsLocation = await this.getCurrentLocation();
+           
+            if(gpsLocation) {
+                const sortedStations = sortStationsByLocation(stations, gpsLocation);
+                this.setState({stations: sortedStations, location: gpsLocation})
+            } else {
+                this.setState({stations: stations, location: null})
+            }
+
+        } catch (error) {
+            console.log(error);
+            this.setState({errorMsg: error.toString()})
+        }
+    }
+
+    async getCurrentLocation() {
+        try {
+            let { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== 'granted') {
+            setErrorMsg('Permission to access location was denied');
+            return false;
+        }
+
+        let location = await Location.getCurrentPositionAsync({});
+        return location;
+        } catch (error) {
+            return error;
+        }
+    };
+    
+    async getData() {
+        try {
+            const data = await supabase
+            .from('gas_station')
+            .select()
+        return data
+        } catch (error) {
+            return error
+        }
+    }
+    
+
     render() {
-        const { navigation } = this.props
+        const { navigation } = this.props;
+        const { stations, errorMsg } = this.state;
+        let list;
+        
+        if(errorMsg) { list = <Text>{errorMsg}</Text>; }
+        else { list = 
+        <View>
+        <GasStationList stations={stations} />
+            <Button 
+                onPress={() => navigation.push('addKilometers')} 
+                title={'Next: Kilometers'}
+                >
+            </Button>
+        </View>; }
         return (
             <KeyboardAvoidingView styles={styles.container}>
                 <Text style={styles.text}>Add Gas Station</Text>
-                <GasStationList />
-                <Button 
-                    onPress={() => navigation.push('addKilometers')} 
-                    title={'Next: Kilometers'}
-                    >
-                </Button>
+                {list}
             </KeyboardAvoidingView>
         );
     }
