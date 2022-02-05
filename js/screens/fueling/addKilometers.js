1c026ccfe1f9e9d71ea5f70f52cef4f55f704d5a
Jannes Brunner <mail@jannesbrunner.de>
2022-02-05 14:05:01 +0100

    func: addKilometers screen

--- js/screens/fueling/addKilometers.js
+++ js/screens/fueling/addKilometers.js
@@ -1,26 +1,89 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, TextInput, View, Button, KeyboardAvoidingView } from 'react-native';
-
+import { parse } from 'react-native-svg';
+import Toast from 'react-native-toast-message';
+import { supabase } from '../../helpers/database';
+import { createLocationString } from '../../helpers/snippets';
 export default class AddKilometersScreen extends Component {
     constructor(props) {
         super(props);
-        this.state = { kilometers: 0 }
-        console.log(props.route)
+        this.state = { 
+            kilometers: 0,
+            lastKilometers: null,
+            correctKilometers: false
+        }
+        this.goToPricePerLiter = this.goToPricePerLiter.bind(this);
+    }
+
+    componentDidMount() {
+        this.getLastKilometers().then(
+            (data) => {
+                if(data) {
+                    this.setState({ kilometers: data[0].mileage, lastKilometers: data[0].mileage })
+                    Toast.show({
+                        type: 'success',
+                        text1: 'Super!',
+                        text2: 'We got the value from your last refueling :)'
+                      });
+                } else {
+                    Toast.show({
+                        type: 'error',
+                        text1: 'Oh no..',
+                        text2: 'Error getting your last km value :('
+                    })
+                }
+            }
+        ).catch( 
+            (error) => { console.log(error)
+            Toast.show({
+                type: 'error',
+                text1: 'Oh no..',
+                text2: error
+            })
+            }
+        )
     }
     
     handleInput(input) {
-        console.log(input);
-        this.setState({ kilometers: input.toString()}) 
+        this.setState({ correctKilometers: true });
+        // do we got a last km value from db?
+        if( this.state.lastKilometers ) {
+            if(parseFloat(input) <= this.state.lastKilometers) {
+                this.setState({ correctKilometers: false });
+            } 
+        }
+        if( parseFloat(input) <= 0 ) this.setState({ correctKilometers: false }); 
+        if( isNaN(input) || input.length == 0) this.setState({ correctKilometers: false });
+        this.setState({ kilometers: input.toString()})
+    }
+
+    async getLastKilometers() {
+        try {
+            const response = await supabase
+            .from('refueling')
+            .select('created_at, mileage')
+            .order('created_at', { ascending: false})
+            .limit(1);
+            const { data, error } = response;
+            if(error) {
+                throw new Error(error.message);
+            } else return data;
+        } catch (error) {
+            console.log(error);
+        }  
+    }
+
+    goToPricePerLiter() {
+        navigation.push('addPricePerLiter',  { location: props.route.params.location, kilometers: this.state.kilometers })
     }
     
     render() {
         const { navigation } = this.props
-        const address = this.props.route.params.location.address;
-        const company = this.props.route.params.location.company;
+        const { correctKilometers: correctMileage } = this.state;
         return (
             <KeyboardAvoidingView styles={styles.container}>
                 <Text style={styles.text}>Add Total Kilometer</Text>
-                <Text>At Station: {company} ({address})</Text>
+                <Text>At Station: {createLocationString(props.route.params.location)}</Text>
                 <TextInput 
                     style={styles.textInput}
                     value={this.state.kilometers.toString()}
@@ -29,8 +92,9 @@ export default class AddKilometersScreen extends Component {
                     onChangeText={(input) => this.handleInput(input)}
                 ></TextInput>
                 <Button 
-                    onPress={() => navigation.push('addPricePerLiter')} 
                     title={'Next: Add Price Per Liter'}
+                    onPress={() => this.goToPricePerLiter()}
+                    disabled={!correctMileage}
                     >
                 </Button>
             </KeyboardAvoidingView>
