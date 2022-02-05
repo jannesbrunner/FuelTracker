ae5cc2065960c5295df77e06a19468027d67c57d
Jannes Brunner <mail@jannesbrunner.de>
2022-02-05 17:52:44 +0100

    func: show saved fuels in table

--- js/screens/manageFuels.js
+++ js/screens/manageFuels.js
@@ -1,59 +1,91 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, ActivityIndicator } from 'react-native';
+import { DataTable } from 'react-native-paper';
 
 import { supabase } from '../helpers/database';
 
 
 export default class manageFuelsScreen extends Component {
-    
+
     constructor(props) {
         super(props);
-        this.state = { 
+        this.state = {
             fuels: [],
-            error: null    
+            error: null,
+            isLoading: true,
         }
     }
 
     render() {
         const { navigation } = this.props
-        const { error, fuels} = this.state
+        const { error, fuels } = this.state
         console.log(fuels);
+
+        var rows = [];
+        for (var i = 0; i < fuels.length; i++) {
+            // note: we are adding a key prop here to allow react to uniquely identify each
+            // element in this array. see: https://reactjs.org/docs/lists-and-keys.html
+            rows.push(
+                <DataTable.Row key={fuels[i].id}>
+                    <DataTable.Cell>{fuels[i].created_at.substring(0, 10)}</DataTable.Cell>
+                    <DataTable.Cell>{fuels[i].gas_station.company}</DataTable.Cell>
+                    <DataTable.Cell>{fuels[i].gas_station.address}</DataTable.Cell>
+                    <DataTable.Cell>{fuels[i].volume}</DataTable.Cell>
+                    <DataTable.Cell>{fuels[i].price}</DataTable.Cell>
+                </DataTable.Row>
+            );
+        }
+
+        const list = <DataTable>
+            <DataTable.Header>
+                <DataTable.Title>Date</DataTable.Title>
+                <DataTable.Title>Station</DataTable.Title>
+                <DataTable.Title>Address</DataTable.Title>
+                <DataTable.Title>Fueled (L)</DataTable.Title>
+                <DataTable.Title>Total Price</DataTable.Title>
+            </DataTable.Header>
+            {rows}
+        </DataTable>;
+        
+        const { isLoading } = this.state;
         return (
             <View styles={styles.container}>
                 <Text style={styles.text}>Manage Fuels</Text>
-                {error ? <Text>There was an error: {error} </Text> : <Text>Your recorded fuelings:</Text>}
-                {fuels.length > 0 ? <Text>{fuels[0].mileage}</Text> : <Text>No fuelings yet</Text> }
-                
-                
-                <Button 
-                    onPress={() => navigation.goBack()} 
+                {error ? <Text>There was an error: {error} </Text> : <Text>Your have {fuels.length} recorded fuelings</Text>}
+                {isLoading ? <ActivityIndicator size="large" /> : list}
+
+
+                <Button
+                    onPress={() => navigation.goBack()}
                     title={'Back'}
-                    >
+                >
                 </Button>
             </View>
         );
     }
 
     async getFuelings() {
-            const { data, error } = await supabase
+        const { data, error } = await supabase
             .from('refueling')
-            .select();
-            if(error) {
-                console.log(error);
-                this.setState({
-                    fuels: [],
-                    error: error.message
-                })
-            } else {
-                console.log(data);
-                this.setState({
-                    fuels: data
-                })
-            }
+            .select('*, gas_station( company, address )');
+        if (error) {
+            console.log(error);
+            this.setState({
+                fuels: [],
+                error: error.message,
+                isLoading: true
+            })
+        } else {
+            console.log(data);
+            this.setState({
+                fuels: data,
+                isLoading: false
+            })
+        }
     }
 
     componentDidMount() {
-       this.getFuelings();
+        this.getFuelings();
     }
 }
 
